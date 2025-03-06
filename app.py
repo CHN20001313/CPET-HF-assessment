d56dfdb8c4681db400d739f9ba42e6a8ecae8143
CHN20001313 <chn2374120915@163.com>
2025-03-06 17:11:52 +0800

    feat(): update code

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ st.set_page_config(layout="wide")
 # 页面标题和简介
 st.title("CPET Based Post-AMI HFrEF/HFmrEF Probability Predictor")
 st.markdown("""
-This tool predicts the likelihood of HFrEF/HFmrEF  after acute myocardial infarction (AMI) based on patient characteristics.
+This tool predicts the likelihood of HFrEF/HFmrEF after acute myocardial infarction (AMI) based on patient characteristics.
 
 **Instructions:**
 - Fill in your details on the left.
@@ -28,25 +28,26 @@ col1, col2 = st.columns(2)
 # 左侧输入区域
 with col1:
     st.header("Input Features")
+    BNP = st.number_input("NT-pro BNP (pg/mL)", min_value=0.0, max_value=100000.0, value=1.0, step=0.1)
     LVEDD = st.number_input("Left Ventricular End-Diastolic Diameter (LVEDD, mm)", min_value=10.0, max_value=100.0, value=45.0, step=0.1)
-    BRRPEAK = st.number_input("Breathing reserve ratio peak(BR peak, %)", min_value=0.0, max_value=100.0, value=30.0, step=0.1)
-    LDH = st.number_input("Lactate dehydrogenase (LDH, U/L)", min_value=0.0, max_value=10000.0, value=120.0, step=0.1)
     VO2KGPEAK = st.number_input("Oxygen consumption peak (VO2 peak, ml/kg/min)", min_value=0.0, max_value=100.0, value=10.0, step=0.1)
-    SBPpeak = st.number_input("Systolic blood pressure (SBP peak, mmHg)", min_value=0.0, max_value=300.0, value=150.0, step=1.0)
+    LDH = st.number_input("Lactate dehydrogenase (LDH, U/L)", min_value=0.0, max_value=10000.0, value=120.0, step=0.1)
     VEVCO2SLOPE = st.number_input("Minute ventilation/carbon dioxide production slope (VE/VCO2 slope)", min_value=0.0, max_value=100.0, value=20.0, step=0.1)
-    VEKGpeak = st.number_input("Ventilation peak (VE peak, ml/kg)", min_value=100.0, max_value=2000.0, value=400.0, step=0.1)
+    CKMB = st.number_input("CK-MB (U/L)", min_value=0.0, max_value=100000.0, value=1.0, step=0.1)
     EF = st.number_input("Ejection fraction (EF, %)", min_value=50.0, max_value=100.0, value=55.0, step=0.1)
     TNI = st.number_input("Troponin I (cTNI, μg/L)", min_value=0.0, max_value=100000.0, value=10.0, step=0.1)
-    CKMB = st.number_input("CK-MB (U/L)", min_value=0.0, max_value=100000.0, value=1.0, step=0.1)
+    PETCO2peak = st.number_input("Peak prtial pressure of end tidal carbon dioxide (PETCO2 peak, mmHg)", min_value=0.0, max_value=100.0, value=40.0, step=0.1)
+    VTpeak = st.number_input("Peak tidal volume (VT peak, L)", min_value=0.0, max_value=10.0, value=2.0, step=0.01)
+
 
     if st.button("Predict"):
         with st.spinner("Calculating..."):
             time.sleep(3)  # 模拟计算时间
         st.success("Calculation complete!")
         # 特征编码
-        feature_names = ["LVEDD", "BR peak", "LDH", "VO2 peak", "SBP peak", "VE/VCO2 slope", "VE peak", "EF", "cTNI", "CKMB"]
+        feature_names = ["NT-pro BNP", "LVEDD", "VO2 peak", "LDH", "VE/VCO2 slope", "CK-MB", "EF", "cTNI", "PETCO2 peak", "VT peak"]
         encoded_features = [
-            LVEDD, BRRPEAK, LDH, VO2KGPEAK, SBPpeak, VEVCO2SLOPE, VEKGpeak, EF, TNI ,CKMB]
+            BNP, LVEDD, VO2KGPEAK, LDH, VEVCO2SLOPE, CKMB, EF, TNI , PETCO2peak, VTpeak]
         input_features = np.array(encoded_features).reshape(1, -1)
         dmatrix = xgb.DMatrix(input_features)
 
@@ -55,13 +56,13 @@ with col1:
         predicted_probability = probabilities[0]
 
         # 风险分组逻辑
-        if predicted_probability < 0.425285:
+        if predicted_probability < 0.248145:
             risk_group = "Low HFrEF/HFmrEF Probability"
             risk_color = "green"
             advice = (
                 "You have a low probability of HFrEF/HFmrEF."
             )
-        elif 0.425285 <= predicted_probability <= 0.544843:
+        elif 0.248145 <= predicted_probability <= 0.338074:
             risk_group = "Medium HFrEF/HFmrEF Probability"
             risk_color = "orange"
             advice = (
@@ -71,7 +72,7 @@ with col1:
         else:
             risk_group = "High HFrEF/HFmrEF Probability"
             risk_color = "red"
-            advice = ("You have a high probability of HFrEF/HFmrEF.Please consult a healthcare professional as soon as possible "
+            advice = ("You have a high probability of HFrEF/HFmrEF. Please consult a healthcare professional as soon as possible "
                 "for detailed evaluation and treatment guidance."
             )
 
